7b8aa930de76223c6b0291755879c5304d5a995a
Bret Hogg <hoggb@wwu.edu>
2017-10-26 18:22:31 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ GROUP BY projects.title HAVING amount_over >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount FROM pledges, projects LEFT OUTER JOIN users ON users.id=pledges.user_id
+"SELECT users.name, pledges.amount FROM pledges, projects LEFT OUTER JOIN users ON users.id=pledges.user_id, projects.id=pledges.project_id
 GROUP BY projects.title ORDER BY pledges.amount, users.name ASC;"
 end
 
